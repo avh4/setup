ac0769e1007de0c3c871335b7288393eac47e90f
Aaron VonderHaar <gruen0aermel@gmail.com>
2010-04-06 00:25:15 -0700

    Script to install git-1.7 on dreamhost.

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir build || exit 1
+cd build
+
+# Install git
+wget http://www.kernel.org/pub/software/scm/git/git-1.7.0.4.tar.gz
+tar zxvf git-1.7.0.4.tar.gz
+cd git-1.7.0.4
+make configure
+./configure --prefix="$HOME" NO_CURL=1
+make
+make install
