336c57c7b7648e431c0b540d901fc525a5d8a3d2
Aaron VonderHaar <gruen0aermel@gmail.com>
2010-08-05 14:29:48 -0700

    Install Google Chrome

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -5,3 +5,20 @@ sudo apt-get install \
 	vim-vimoutliner \
 	git-core git-svn git-doc gitk \
 	remind
+
+SOURCES=/etc/apt/sources.list.d/avh4.list
+sudo cp avh4.list /etc/apt/sources.list.d/
+
+function install_key {
+	echo "Installing key for $1 ($2)..."
+	gpg --keyserver pgp.mit.edu --recv-keys $2
+	gpg --export -a $2 | sudo apt-key add -
+}
+
+install_key Google A040830F7FAC5991
+
+sudo apt-get update
+
+sudo apt-get install \
+	google-chrome-stable
+
