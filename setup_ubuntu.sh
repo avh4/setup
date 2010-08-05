ce9d869d60a3158d76f65f59655e8212776fb9b2
Aaron VonderHaar <gruen0aermel@gmail.com>
2010-08-05 14:32:43 -0700

    Configure git

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -15,9 +15,9 @@ function install_key {
 	gpg --export -a $2 | sudo apt-key add -
 }
 
-install_key Google A040830F7FAC5991
 install_key Virtualbox 54422A4B98AB5139
 install_key Dropbox FC918B335044912E
+install_key Google A040830F7FAC5991
 
 sudo apt-get update
 
@@ -26,3 +26,5 @@ sudo apt-get install \
 	virtualbox-3.2 \
 	nautilus-dropbox
 
+git config --global --add merge.conflictstyle diff3
+git config --global --add color.ui auto
